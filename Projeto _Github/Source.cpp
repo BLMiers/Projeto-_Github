b53b702ae9518d77893854d3c26326c1b04f734a
brian.samburgo <brian.samburgo@LAD0221>
2017-09-14 18:40:37 -0300

    Add project files.

---
+++ Projeto _Github/Source.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main() {
+
+	cout << "TAMO FODIDOOOOOOOOOOOOOOOOOOOOOOOOO!!!!!!!!!!" << endl;
+
+	system("PAUSE");
+	return 0;
+}
